9886ac4f82c84588b55a234e4849737950214147
jabir149 <reacthemejabirs@gmail.com>
2026-01-13 15:35:01 +0600

    testing

--- mailer.php
+++ mailer.php
@@ -1,47 +1,44 @@
 <?php
-// Only process POST reqeusts.
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form fields and remove whitespace.
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Sanitize form fields
     $name = strip_tags(trim($_POST["name"]));
     $name = str_replace(array("\r", "\n"), array(" ", " "), $name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $message = trim($_POST["message"]);
+    $query = htmlspecialchars(trim($_POST["query"]));
+    $message = htmlspecialchars(trim($_POST["message"]));
 
-    // Check that data was sent to the mailer.
-    if (empty($name) or empty($message) or !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Set a 400 (bad request) response code and exit.
+    // Validate required fields
+    if (empty($name) || empty($message) || empty($query) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         http_response_code(400);
-        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        echo "Oops! Please complete all required fields correctly.";
         exit;
     }
 
-    // Set the recipient email address.
-    // FIXME: Update this to your desired email address.
+    // Recipient email
     $recipient = "support@reactheme.com";
 
-    // Set the email subject.
-    $subject = "Bizgen Contact Form $name";
+    // Email subject
+    $subject = "Bizgen Contact Form Submission: $name";
 
-    // Build the email content.
+    // Build email content
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n";
+    $email_content .= "Query: $query\n";
     $email_content .= "Message:\n$message\n";
 
-    // Build the email headers.
+    // Email headers
     $email_headers = "From: $name <$email>";
 
-    // Send the email.
+    // Send email
     if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code.
         http_response_code(200);
         echo "Thank You! Your message has been sent.";
     } else {
-        // Set a 500 (internal server error) response code.
         http_response_code(500);
         echo "Oops! Something went wrong and we couldn't send your message.";
     }
 } else {
-    // Not a POST request, set a 403 (forbidden) response code.
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
 }
+?>
